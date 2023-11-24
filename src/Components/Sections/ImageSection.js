2fdf0a284adbfdf53017afa9ef3d19d0fbd798a4
45afeef <45afeef@gmail.com>
2023-11-24 19:50:04 +0530

    Section Tilte - Varient sizes added

--- src/Components/Sections/ImageSection.js
+++ src/Components/Sections/ImageSection.js
@@ -1,4 +1,5 @@
 import Counter from '../Counter';
+import SectionTitle from './Title';
 import s from './imagesection.module.css'
 
 const ImageSection = ({
@@ -12,25 +13,23 @@ const ImageSection = ({
                 color: color
             }}>
             <div className={s.row}>
-                <h4 className={s.sectionTitle}>
-                    Some intresting facts
-                </h4>
+                <SectionTitle color="white" underLineSize='large'>Some intresting facts</SectionTitle>
                 <div className={s.content}>
                     <p>
-                    Our team is holistically equipped, to deliver excellence across the entire spectrum of design consultancy services. From ideation and conceptualization, to design development and implementation, we execute with finesse, on the strength of our varied expertise. While skilfully integrating unique and creative artistic elements with utter functionality, we also strive to assimilate the best of tradition in all our projects. Unconditional respect for the preferences and aspirations of our clients motivates us to innovate and create unique designs.
+                        Our team is holistically equipped, to deliver excellence across the entire spectrum of design consultancy services. From ideation and conceptualization, to design development and implementation, we execute with finesse, on the strength of our varied expertise. While skilfully integrating unique and creative artistic elements with utter functionality, we also strive to assimilate the best of tradition in all our projects. Unconditional respect for the preferences and aspirations of our clients motivates us to innovate and create unique designs.
                     </p>
                     <div className={s.counters}>
                         {[{
-                            count:123,
-                            text:"Completed Projects"
-                        },{
-                            count:59,
-                            text:"Happy Customers"
-                        },{
-                            count:8926,
-                            text:"Working hours"
+                            count: 123,
+                            text: "Completed Projects"
+                        }, {
+                            count: 59,
+                            text: "Happy Customers"
+                        }, {
+                            count: 8926,
+                            text: "Working hours"
                         }
-                        ].map((count,index) => <Counter key={index} {...count} />)}
+                        ].map((count, index) => <Counter key={index} {...count} />)}
                     </div>
                 </div>
             </div>
--- src/Components/Sections/Title.js
+++ src/Components/Sections/Title.js
@@ -1,15 +1,41 @@
 import s from './title.module.css'
 
-const SectionTitle = ({ children, color = "black" }) => {
+const SectionTitle = ({
+    children,
+    color = "black",
+    underLineSize = "normal"
+}) => {
+
+    // Determine the necessary width and margin based on size for underline
+    let underLineWidth;
+    let underLineMargin;
+    switch (underLineSize) {
+        case "small":
+            underLineWidth = "10%";
+            underLineMargin = "0px";
+            break;
+        case "large":
+            underLineWidth = "50%";
+            underLineMargin = "80px";
+            break;
+        case "normal":
+        default:
+            underLineWidth = "25%";
+            underLineMargin = "20px";
+            break;
+    }
+
     return (
-        <h2
+        <h4
             className={s.sectionTitle}
             style={{
-                '--color': color
+                '--color': color,
+                '--underLineWidth': underLineWidth,
+                '--underLineMargin': underLineMargin,
             }}
         >
             {children}
-        </h2>
+        </h4>
     )
 }
 
